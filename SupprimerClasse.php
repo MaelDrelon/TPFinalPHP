5bdc1025843622e2b51a152d37067e8b8c472c0d
MaelDrelon <70696368+MaelDrelon@users.noreply.github.com>
2021-01-05 14:42:42 +0100

    upd

---
+++ SupprimerClasse.php
@@ -0,0 +1,9 @@
+<?php 
+include "fonction.php";
+menu();
+?>
+
+<head>
+  <link rel="stylesheet" href="index.css">
+</head>
+<div class=fond>
--- fonction.php
+++ fonction.php
@@ -9,44 +9,26 @@ $maBase= new PDO('mysql:host=192.168.65.227; dbname=TPFinalPHPRabasteDrelon; cha
           <ul class="sous">
             <li><a href="ListeEleve.php">Liste Eleves</a></li>
             <li><a href="AjouterEleve.php">Ajouter Eleve</a></li>
+            <li><a href="SupprimerEleve.php">Supprimer Eleve</a></li>
           </ul>
         </li>
         <li class="deroulant"><a href="">Professeurs &ensp;</a>
           <ul class="sous">
             <li><a href="ListeProf.php">Liste Professeurs</a></li>
             <li><a href="AjouterProf.php">Ajouter Professeur</a></li>
+            <li><a href="SupprimerProf.php">Supprimer Professeur</a></li>
           </ul>
         </li>
         <li class="deroulant"><a href="">Classes</a>
           <ul class="sous">
             <li><a href="ListeClasse.php">Liste Classes</a></li>
             <li><a href="AjouterClasse.php">Ajouter Classe</a></li>
+            <li><a href="SupprimerClasse.php">Supprimer Classe</a></li>
           </ul>
-        </li>
-        <li><a href="Gestion.php">Gestion</a>
         </li>
       </ul>
     </nav>
   </p>
 <?php
     }
-
-    /*function delete($maBase){
-      if(isset($_POST['Reponse']) == "Oui") 
-        {
-          if ((!empty($_POST['Reponse']))
-          {
-            $del = $maBase->query();
-          }
-        }
-        
-          
-      echo "Supprimer? Ecriver Oui";
-      ?>
-      <form action="" method="post">
-      <p>Nom: <input type="text" name="Reponse" /></p>
-      <p><input type="submit" value="Envoyer"/></p>
-      </form>
-      <?php
-    }*/
 ?>
